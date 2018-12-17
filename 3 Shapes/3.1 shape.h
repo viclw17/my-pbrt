259871a8d4208c9f5a618a44cc8eec738908b5e9
viclw17 <viclw17@gmail.com>
2018-12-16 17:27:07 -0800

    Reading.

--- 3 Shapes/3.1 shape.h
+++ 3 Shapes/3.1 shape.h
@@ -2,19 +2,22 @@
 class Shape {
   public:
     // Shape Interface
-    Shape(const Transform *ObjectToWorld, const Transform *WorldToObject,
-          bool reverseOrientation);
+    Shape(const Transform *ObjectToWorld, const Transform *WorldToObject, bool reverseOrientation);
     virtual ~Shape();
 
     virtual Bounds3f ObjectBound() const = 0;
     virtual Bounds3f WorldBound() const;
 
     virtual bool Intersect(const Ray &ray, Float *tHit, SurfaceInteraction *isect, bool testAlphaTexture = true) const = 0;
+    // checks for a ray–box intersection and returns the two parametric t values
+    // of the intersection, if any.
     virtual bool IntersectP(const Ray &ray, bool testAlphaTexture = true) const {
         return Intersect(ray, nullptr, nullptr, testAlphaTexture);
     }
-
+    // In order to properly use Shapes as area lights,
+    // it is necessary to be able to compute the surface area of a shape in object space.
     virtual Float Area() const = 0;
+
     // Sample a point on the surface of the shape and return the PDF with
     // respect to area on the surface.
     virtual Interaction Sample(const Point2f &u, Float *pdf) const = 0;
